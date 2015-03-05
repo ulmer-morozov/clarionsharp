91416f093a8e8eb171214e234ccfb947a1c85c98
ulmermorozov <t*KVW574HQZ>
2015-03-05 18:11:38 +0300

    Added support for Clarion Date Convertion

---
+++ ClarionSharp/ClarionDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClarionSharp
+{
+    public static class ClarionDate
+    {
+        public static DateTime GetDateTime(int clarionDate)
+        {
+            var date = new DateTime(1800, 12, 28, 0, 0, 0);
+            date = date.AddDays(clarionDate);
+            return date;
+        }
+    }
+}
